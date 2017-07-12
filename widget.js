c3e32b7e02d5d1ef3ed6dbe60235c29c49fa94a4
Zack Bloom <zackbloom@gmail.com>
2017-07-12 13:24:53 -0700

    Fix iPhone 7 mobile rendering issues

--- widget.js
+++ widget.js
@@ -156,7 +156,7 @@
 
     _bftn_util.setCookie('_BFTN_WIDGET_SHOWN', 'true', 365);
 
-    _bftn_util.injectCSS('_bftn_iframe_css', '#_bftn_wrapper { position: fixed; left: 0px; top: 0px; width: 100%; height: 100%; z-index: 20000; -webkit-overflow-scrolling: touch; overflow-y: scroll; } #_bftn_iframe { width: 100%; height: 100%;  }');
+    _bftn_util.injectCSS('_bftn_iframe_css', '#_bftn_wrapper { position: fixed; left: 0px; top: 0px; width: 100vw; height: 100%; z-index: 20000; -webkit-overflow-scrolling: touch; overflow-y: scroll; } #_bftn_iframe { width: 100%; width: 100vw; height: 100%; min-height: 750px;  }');
 
     // Preload images before showing the animation
     // preload();
